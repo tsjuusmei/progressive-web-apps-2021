71a04a9873aad38a107b9daf87f0b47bd0b773c0
tsjuusmei <guusmay2@hotmail.com>
2021-03-09 11:45:32 +0100

    ejs init with dependencies

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.set('view-engine', 'ejs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home.ejs')
+})
+  
+app.listen(port, () => {
+  console.log('Application running on localhost:' + port)
+})
