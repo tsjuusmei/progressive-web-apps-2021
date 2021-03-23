55092911081bc24222db79f059032c03df6e9bb6
tsjuusmei <guusmay2@hotmail.com>
2021-03-23 14:33:50 +0100

    moved folders

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -3,9 +3,9 @@ const concat = require('gulp-concat')
 const cleanCSS = require('gulp-clean-css')
 
 gulp.src([
-  "./static/styles/main.css",
-  "./static/styles/detail.css"
+  "./src/styles/main.css",
+  "./src/styles/detail.css"
 ])
   .pipe(concat("index.css"))
   .pipe(cleanCSS({compatibility: 'ie8'}))
-  .pipe(gulp.dest("./public/css"))
+  .pipe(gulp.dest("./static/css"))
