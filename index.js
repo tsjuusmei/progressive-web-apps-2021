896d69e0965b22b62ea650e24426cf7324b5c285
tsjuusmei <guusmay2@hotmail.com>
2021-03-23 15:44:48 +0100

    https use and added background to manifest

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ require("dotenv").config();
 const app = express()
 const templates = path.join(__dirname, './src/views')
 
-app.set('view engine', 'ejs')
-app.set('views', templates)
+app
+  .set('view engine', 'ejs')
+  .set('views', templates)
+  .use(function (request, response, next) {
 
-app.use(express.static("static"));
-app.use('/', router)
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+      return response.redirect("https://" + request.headers.host + request.url);
+    }
 
-app.listen(process.env.PORT || 3000, () => console.log("Server is running... on port 3000 or Heroku_port"));
+    next();
+  })
+  .use(express.static("static"))
+  .use('/', router)
+  .listen(process.env.PORT || 3000, () => console.log("Server is running... on port 3000 or Heroku_port"))
